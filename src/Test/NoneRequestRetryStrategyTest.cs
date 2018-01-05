221e6001004bed260d58ba60e18b7fcea568165a
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-01-05 04:03:14 +0100

    Add NoneRequestRetryStrategy

---
+++ src/Test/NoneRequestRetryStrategyTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace PatchKit.Network
+{
+    public class NoneRequestRetryStrategyTest
+    {
+        [Test]
+        public void ShouldRetryIsAlwaysFalse_For_NewInstance()
+        {
+            var strategy = new NoneRequestRetryStrategy();
+            Assert.IsFalse(strategy.ShouldRetry);
+        }
+        
+        [Test]
+        public void ShouldRetryIsAlwaysFalse_For_OnRequestFailure()
+        {
+            var strategy = new NoneRequestRetryStrategy();
+            strategy.OnRequestFailure();
+            Assert.IsFalse(strategy.ShouldRetry);
+        }
+    }
+}
