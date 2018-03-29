3d2cb725a3673f3a0e2246cc1355a96b911934a8
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-03-30 00:00:18 +0200

    Update version

--- src/PatchKit.Network/Properties/Version.cs
+++ src/PatchKit.Network/Properties/Version.cs
@@ -3,8 +3,8 @@
     internal static class Version
     {
         public const string Major = "1";
-        public const string Minor = "0";
-        public const string Patch = "1";
+        public const string Minor = "1";
+        public const string Patch = "0";
         public const string Suffix = "";
     }
 }
